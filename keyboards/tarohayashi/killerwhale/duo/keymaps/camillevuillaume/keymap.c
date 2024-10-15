716a3980290590be85682ed3f0dcb6dde745e716
camillevuillaume <camillevuillaume@gmail.com>
2024-10-15 12:03:50 +0900

    Modified to Sofle like keymap

--- keyboards/tarohayashi/killerwhale/duo/keymaps/camillevuillaume/keymap.c
+++ keyboards/tarohayashi/killerwhale/duo/keymaps/camillevuillaume/keymap.c
@@ -11,8 +11,9 @@ enum layer_number {
     BASE = 0,
     ONOFF, OFFON, ONON,                       // トグルスイッチで変更するレイヤー
 //    LOWER, UPPER, UTIL,                       // 長押しで変更するレイヤー
-    MOUSE, BALL_SETTINGS, LIGHT_SETTINGS, // 自動マウスレイヤー切り替えや設定用のレイヤー
-    NAV, SYM, NUM                           // レイヤー切り替え用のレイヤー
+    NAV, SYM, UTIL,                       // 長押しで変更するレイヤー
+    MOUSE, BALL_SETTINGS, LIGHT_SETTINGS,     // 自動マウスレイヤー切り替えや設定用のレイヤー
+//    NAV, SYM                                  // レイヤー切り替え用のレイヤー
 };
 
 // キーマップの設定
@@ -22,10 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // 天面スイッチ
         KC_ESC,  KC_1,          KC_2,           KC_3,           LT(BALL_SETTINGS, KC_4),    LT(LIGHT_SETTINGS, KC_5),
         KC_TAB,  KC_Q,          KC_W,           KC_E,           KC_R,                       KC_T,
-        KC_LSFT, LGUI_T(KC_A),  LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),               KC_G,
-        KC_Z,    KC_X,          KC_C,           KC_V,           KC_B,                       KC_BSLS ,
+//        KC_LSFT, LGUI_T(KC_A),  LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),               KC_G,
+        KC_LSFT, KC_A,          KC_S,           KC_D,           KC_F,                       KC_G,
+        KC_Z,    KC_X,          KC_C,           KC_V,           KC_B,                       KC_LCTL ,
         // 側面スイッチ
-        MO(MOUSE), LT(NAV, KC_SPACE),               
+        MO(SYM), KC_SPACE,               
         // 十字キーorジョイスティック               // ジョイスティックスイッチ
         KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,      KC_ENT,      
         // 追加スイッチ                           // トグルスイッチ
@@ -33,11 +35,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // 右手
         KC_6, KC_7,             KC_8,           KC_9,           KC_0,                       KC_BSPC,
         KC_Y, KC_U,             KC_I,           KC_O,           KC_P,                       KC_QUOT,
-        KC_H, RSFT_T(KC_J),     RCTL_T(KC_K),   RALT_T(KC_L),   RGUI_T(KC_SCLN),            KC_SLSH,
-        KC_N, KC_M,             KC_COMM,        KC_DOT,         KC_RBRC,                    KC_LBRC,
-        LT(SYM, KC_ENT), LT(NUM, KC_BSPC),                   
+//        KC_H, RSFT_T(KC_J),     RCTL_T(KC_K),   RALT_T(KC_L),   RGUI_T(KC_SCLN),            KC_SLSH,
+        KC_H, KC_J,             KC_K,           KC_L,           KC_SCLN,                    KC_SLSH,
+        KC_N, KC_M,             KC_COMM,        KC_DOT,         KC_RCTL,                    KC_RSFT, 
+        KC_ENT, MO(MOUSE),                   
         KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,      KC_ENT,     
-        CMD_CTL, MO(MOUSE),                     MO(OFFON)
+        KC_RGUI, KC_RALT,                     MO(OFFON)
     ),
     [MOUSE] = LAYOUT(
         //left hand
@@ -118,25 +121,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [SYM] = LAYOUT(
         // 左手
         // 天面スイッチ
-        _______,  _______,      _______,        _______,        _______,                    _______,
-        _______,  LSFT(KC_LBRC),LSFT(KC_7),     LSFT(KC_8),     LSFT(KC_9),                 LSFT(KC_RBRC),
-        _______,  LSFT(KC_SCLN),LSFT(KC_4),     LSFT(KC_5),     LSFT(KC_6),                 LSFT(KC_EQL),
-        LSFT(KC_GRV), LSFT(KC_1),LSFT(KC_2),    LSFT(KC_3),     LSFT(KC_BSLS),              _______,
+        KC_GRV,   KC_F1,        KC_F2,          KC_F3,          KC_F4,                      KC_F5,
+        _______,  KC_1,         KC_2,           KC_3,           KC_4,                       KC_5,
+        _______,  LSFT(KC_1),   LSFT(KC_2),     LSFT(KC_3),     LSFT(KC_4),                 LSFT(KC_5),
+        KC_EQL,   KC_MINS,      LSFT(KC_EQL),   LSFT(KC_LBRC),  LSFT(KC_RBRC),              _______,
         // 側面スイッチh
-        LSFT(KC_MINS), LSFT(KC_0),               
+        _______,  _______,               
         // 十字キーorジョイスティック               // ジョイスティックスイッチ
         _______, _______, _______, _______,     _______,      
         // 追加スイッチ                           // トグルスイッチ
         _______, _______,                 _______,
         // 右手
-        _______,  _______,      _______,        _______,        _______,                    _______,
-        _______,  _______,      _______,        _______,        _______,                    _______,
-        _______,  _______,      _______,        _______,        _______,                    _______,
-        _______,  _______,      _______,        _______,        _______,                    _______,
+        KC_F6,    KC_F7,        KC_F8,          KC_F9,          KC_F10,                     KC_F11,
+        KC_6,     KC_7,         KC_8,           KC_9,           KC_0,                       KC_BSLS,
+        LSFT(KC_6), LSFT(KC_7), LSFT(KC_8),     LSFT(KC_9),     LSFT(KC_0),                 LSFT(KC_BSLS),
+        KC_LBRC,  KC_RBRC,      KC_SCLN,        LSFT(KC_SCLN),  KC_BSLS,                _______,
         _______,  _______,                     
         _______,  _______, _______, _______,     _______,
         _______,  _______,                      _______
-    ),
+    )
+    /*
     [NUM] = LAYOUT(
         // 左手
         // 天面スイッチ
@@ -158,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,                     
         _______,  _______, _______, _______,     _______,
         _______,  _______,                      _______
-    )
+    )*/
 
 };
 
@@ -212,7 +216,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
         ENCODER_CCW_CW(KC_UP, KC_DOWN),
         ENCODER_CCW_CW(KC_WH_U, KC_WH_D),
         ENCODER_CCW_CW(KC_WH_L, KC_WH_R)
-    },
+    }
+    /*
     [NUM] =   { 
         ENCODER_CCW_CW(KC_ESC, KC_TAB),
         ENCODER_CCW_CW(REDO, UNDO),
@@ -222,7 +227,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
         ENCODER_CCW_CW(KC_UP, KC_DOWN),
         ENCODER_CCW_CW(KC_WH_U, KC_WH_D),
         ENCODER_CCW_CW(KC_WH_L, KC_WH_R)
-    }
+    }*/
 };
 
 // 初期化関係
